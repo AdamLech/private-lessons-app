546f86622654a5f1eb06299eb3127bffc9e5466c
Amogus <adammichallech@gmail.com>
2023-07-02 11:43:43 +0200

    created additional structure parts and login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./css/login-page-style.css";
 
 const Login = () => {
   return (
     <>
-      <div>Cwele</div>
+      <div className="page-body">
+        <div className="top-box">
+          <img src="https://via.placeholder.com/100" alt="example image" />
+          <h3>Hello!</h3>
+          <h5>It's nice to see you</h5>
+        </div>
+        <div className="bottom-box">
+          <div>
+            <input type="text" placeholder="Email" />
+            <span class="underline"></span>
+          </div>
+          <div>
+            <input type="password" placeholder="Password" />
+            <span class="underline"></span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
