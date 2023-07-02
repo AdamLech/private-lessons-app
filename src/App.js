096642a9e717061d0a1e8091e5af6516b69dcbaa
Amogus <adammichallech@gmail.com>
2023-07-02 11:04:46 +0200

    Created basic app structure in react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Login from "./components/LoginPage";
+
+function App() {
+  return <></>;
+}
+
+export default App;
